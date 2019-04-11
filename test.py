3261a36b6f695c74dda471d975c204c543bf3c4c
cs91chris <cs91chris@voidbrain.me>
2019-04-11 18:51:33 +0200

    added tests for dispatch

--- test.py
+++ test.py
@@ -36,20 +36,30 @@ def client(app):
 
 
 def test_app_runs(client):
-    res = client.get('/')
+    res = client.get('/web')
     assert res.status_code == 200
 
 
 def test_app_return_html(client):
-    res = client.get('/')
+    res = client.get('/web')
     assert 'text/html' in res.headers['Content-Type']
 
 
 def test_app_returns_json(client, app):
-    res = client.get('/', 'http://api.' + app.config['SERVER_NAME'])
+    res = client.get('/', base_url='http://api.' + app.config['SERVER_NAME'])
     assert res.headers['Content-Type'] == 'application/json'
 
 
 def test_api_cors(client, app):
     res = client.get('/', base_url='http://api.' + app.config['SERVER_NAME'])
     assert res.headers['Access-Control-Allow-Origin'] == '*'
+
+
+def test_dispatch_error_web(client):
+    res = client.get('/web-page-not-found')
+    assert res.status_code == 404 and 'text/html' in res.headers['Content-Type']
+
+
+def test_dispatch_error_api(client, app):
+    res = client.get('/api-not-found', base_url='http://api.' + app.config['SERVER_NAME'])
+    assert res.status_code == 404 and res.headers['Content-Type'] == 'application/json'
