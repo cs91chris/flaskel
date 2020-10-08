2fd1c2089ed461283122b23abe3a648371622087
cs91chris <cs91chris@voidbrain.me>
2020-10-08 22:35:05 +0200

    remove unnecessary stuff from skeleton

--- skeleton/blueprints/api/__init__.py
+++ skeleton/blueprints/api/__init__.py
@@ -2,15 +2,9 @@
 #
 from flask import Blueprint
 
-from .index import APIResource
 from flaskel.ext import cors, errors
 
-api = Blueprint(
-    'api',
-    __name__,
-    subdomain='api'
-)
+api = Blueprint('api', __name__, subdomain='api')
 
 cors.init_app(api)
 errors.api_register(api)
-APIResource.register(api, 'resource_api', '/resources')
