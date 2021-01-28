48a04b77bd0ceb484fbfc9c48f9942af426b9817
cs91chris <cs91chris@voidbrain.me>
2021-01-28 17:29:08 +0100

    add paging and filter to model

--- flaskel/ext/sqlalchemy/model.py
+++ flaskel/ext/sqlalchemy/model.py
@@ -3,15 +3,18 @@ from flask_sqlalchemy import Model
 
 class SQLAModel(Model):
     @classmethod
-    def get_one(cls, raise_not_found=True, to_dict=True, **kwargs):
+    def get_one(cls, raise_not_found=True, to_dict=True, *args, **kwargs):
         """
 
         :param raise_not_found:
         :param to_dict:
-        :param kwargs:
         :return:
         """
-        res = cls.query.filter_by(**kwargs)
+        if args:
+            res = cls.query.filter(*args)
+        else:
+            res = cls.query.filter_by(**kwargs)
+
         if raise_not_found:
             res = res.first_or_404()
         else:
@@ -25,17 +28,27 @@ class SQLAModel(Model):
             return res
 
     @classmethod
-    def get_list(cls, to_dict=True, order_by=None, **kwargs):
+    def get_list(cls, to_dict=True, order_by=None, page=None, page_size=None, *args, **kwargs):
         """
 
         :param to_dict:
         :param order_by:
-        :param kwargs:
+        :param page:
+        :param page_size:
         :return:
         """
-        q = cls.query.filter_by(**kwargs)
+        if args:
+            q = cls.query.filter(*args)
+        else:
+            q = cls.query.filter_by(**kwargs)
+
         if order_by is not None:
             q = q.order_by(order_by)
+
+        if page_size:
+            q = q.limit(page_size)
+        if page:
+            q = q.offset(page * page_size)
 
         res = q.all()
 
