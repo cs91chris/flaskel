d0542b0db2ebd68cad64c5d91595735a8a064b5a
cs91chris <cs91chris@voidbrain.me>
2021-02-15 01:23:18 +0100

    customization of webargs fields

--- flaskel/utils/webargs.py
+++ flaskel/utils/webargs.py
@@ -1,13 +1,29 @@
 import functools
 
 import flask
-from webargs import flaskparser
+from webargs import fields, flaskparser
 
-from flaskel import httpcode
+from flaskel import config, httpcode
 
 parser = flaskparser.FlaskParser()
 query = functools.partial(parser.use_args, location="query")
 payload = functools.partial(parser.use_args, location="json")
+
+
+class Fields:
+    integer = fields.Integer
+    string = fields.String
+    decimal = fields.Decimal
+    boolean = functools.partial(fields.Boolean, truthy={'true', 1}, falsy={'false', 0})
+    positive = functools.partial(fields.Integer, validate=lambda x: x > 0)
+    not_negative = functools.partial(fields.Integer, validate=lambda x: x >= 0)
+    not_positive = functools.partial(fields.Integer, validate=lambda x: x <= 0)
+    negative = functools.partial(fields.Integer, validate=lambda x: x < 0)
+    isodate = functools.partial(fields.DateTime, format=config.DATE_ISO_FORMAT)
+
+    @classmethod
+    def list_of(cls, elem_field=None, delimiter='+', **kwargs):
+        return fields.DelimitedList(elem_field or cls.string, delimiter=delimiter, **kwargs)
 
 
 # noinspection PyUnusedLocal
