d7858d28eea63e6bc74af3279c4d8ac5e7ff9ee8
cs91chris <cs91chris@voidbrain.me>
2021-04-21 01:56:11 +0200

    add file lock to scheduler on multiprocess env

--- flaskel/ext/jobs.py
+++ flaskel/ext/jobs.py
@@ -1,3 +1,4 @@
+import atexit
 import logging
 
 from apscheduler import events
@@ -6,9 +7,23 @@ from flask_apscheduler import APScheduler
 
 from flaskel.utils.uuid import get_uuid
 
+try:
+    import fcntl
+except ImportError:
+    fcntl = None
+
 
 class APJobs(APScheduler):
     def init_app(self, app, test_sync=True):
+        app.config.setdefault('SCHEDULER_AUTO_START', False)
+        app.config.setdefault('SCHEDULER_PATCH_MULTIPROCESS', True)
+
+        if app.config.SCHEDULER_PATCH_MULTIPROCESS is True:
+            if fcntl is None:
+                app.logger.warning('fcntl not supported on this platform')
+            elif not self._set_lock():
+                return  # pragma: no cover
+
         super().init_app(app)
 
         # if app is in testing mode execute tasks synchronously
@@ -20,8 +35,6 @@ class APJobs(APScheduler):
             logger.setLevel(logging.DEBUG)
             logger = logging.getLogger('flask_apscheduler')
             logger.setLevel(logging.DEBUG)
-
-        app.config.setdefault('SCHEDULER_AUTO_START', False)
 
         if not hasattr(app, 'extensions'):
             app.extensions = dict()  # pragma: no cover
@@ -40,8 +53,37 @@ class APJobs(APScheduler):
         kwargs = kwargs or {}
         kw['kwargs'] = kwargs
         job_id = get_uuid()
+        job = super().add_job(id=job_id, func=func, **kw)
         self.app.logger.debug(f'added job {func}: {job_id}')
-        return super().add_job(id=job_id, func=func, **kw)
+        return job
+
+    @staticmethod
+    def _set_lock():  # pragma: no cover
+        """
+        ensure that only one worker starts the scheduler
+        :return: (boolean) True -> scheduler can start
+        """
+        f = None
+
+        def unlock():
+            try:
+                fcntl.flock(f, fcntl.LOCK_UN)
+                f.close()
+            except OSError:
+                pass
+
+        try:
+            f = open(".scheduler.lock", "wb")
+            fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            atexit.register(unlock)
+            return True
+        except OSError:
+            if f is not None:
+                try:
+                    f.close()
+                except OSError:
+                    pass
+        return False
 
 
 scheduler = APJobs()
