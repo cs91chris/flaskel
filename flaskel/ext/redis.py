bacb2fb50304ffb6b08999105d0c314e1840fd2d
cs91chris <cs91chris@voidbrain.me>
2021-07-07 17:46:37 +0200

    add support to redis json

--- flaskel/ext/redis.py
+++ flaskel/ext/redis.py
@@ -1,8 +1,36 @@
-# pragma: no cover
+from flask_response_builder.builders.encoders import JsonEncoder
+
 try:
     import redis
-except ImportError:
+except ImportError:  # pragma: no cover
     redis = None
+
+try:
+    from rejson import Client, Path
+except ImportError:  # pragma: no cover
+    Client, Path = object
+
+
+class JSONRedisClient(Client):
+    def __init__(self, encoder=None, *args, **kwargs):
+        super().__init__(encoder or JsonEncoder(), *args, **kwargs)
+
+    @staticmethod
+    def json_path(path=None):
+        return Path(path) if path else Path.rootPath()
+
+    def json_set(self, key, data, path=None):
+        return self.jsonset(key, self.json_path(path), data)
+
+    def json_get(self, key, path=None):
+        return self.jsonget(key, self.json_path(path))
+
+    def json_array_add(self, key, data, path=None, **kwargs):
+        path = self.json_path(path)
+        pipe = self.pipeline()
+        pipe.jsonset(key, path, [], nx=True)
+        pipe.jsonarrappend(key, path, data)
+        return pipe.execute()[1]
 
 
 class FlaskRedis:
@@ -13,29 +41,34 @@ class FlaskRedis:
         :param client:
         :param kwargs:
         """
-        self._opts = kwargs
         self._client = client
 
         if app is not None:
-            self.init_app(app)  # pragma: no cover
+            self.init_app(app, **kwargs)  # pragma: no cover
 
     @property
     def client(self):
-        return self._client   # pragma: no cover
+        return self._client  # pragma: no cover
 
-    def init_app(self, app):
+    def init_app(self, app, redis_class=None, **kwargs):
         """
 
         :param app:
+        :param redis_class:
         """
         assert redis is not None, "redis must be installed"
+        if redis_class is None:
+            if Client is object:
+                redis_class = redis.StrictRedis
+            else:
+                redis_class = JSONRedisClient
 
         app.config.setdefault("REDIS_URL", "redis://localhost:6379/0")
-        app.config.setdefault("REDIS_OPTS", self._opts)
+        app.config.setdefault("REDIS_OPTS", **kwargs)
 
         if not self._client:
             # noinspection PyUnresolvedReferences
-            self._client = redis.StrictRedis.from_url(
+            self._client = redis_class.from_url(
                 app.config["REDIS_URL"], **app.config["REDIS_OPTS"]
             )
 
