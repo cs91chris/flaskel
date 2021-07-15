1ef345d9644d885a7e5ea5733ad035250079a5bd
cs91chris <cs91chris@voidbrain.me>
2021-07-15 22:43:28 +0200

    set default options to mongo extension

--- flaskel/ext/mongo.py
+++ flaskel/ext/mongo.py
@@ -16,11 +16,14 @@ class FlaskMongoDB(PyMongo):
         :param args:
         :param kwargs:
         """
-        assert PyMongo is not object
+        assert PyMongo is not object, "you must install 'flask_pymongo'"
 
         app.config.setdefault('MONGO_OPTS', {})
+        app.config['MONGO_OPTS'].setdefault('connectTimeoutMS', 1000)
+        app.config['MONGO_OPTS'].setdefault('serverSelectionTimeoutMS', 1000)
         app.config['MONGO_OPTS'].update(**kwargs)
 
+        # noinspection PyUnresolvedReferences
         super().init_app(app, uri, *args, **kwargs)
         app.extensions['mongo'] = ObjectDict()
         app.extensions['mongo'][ext_name] = self
