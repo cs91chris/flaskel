31d864c5a37ddfb38550bee8bcb1efe764c81763
cs91chris <cs91chris@voidbrain.me>
2021-08-21 21:53:24 +0200

    added flask socketio extension

---
+++ flaskel/ext/websocket/__init__.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+try:
+    from flask_socketio import SocketIO
+
+    IO_ASYNC_MODE = config("IO_ASYNC_MODE", default=None)
+    IO_CORS_ORIGINS = config("IO_CORS_ORIGINS", default="*")
+    IO_LOG_ENABLED = config("IO_LOG_ENABLED", default=True, cast=bool)
+
+    socketio = SocketIO(
+        async_mode=IO_ASYNC_MODE,
+        logger=IO_LOG_ENABLED,
+        engineio_logger=IO_LOG_ENABLED,
+        cors_allowed_origins=IO_CORS_ORIGINS,
+    )
+except (ModuleNotFoundError, ImportError):
+    socketio = None
---
+++ flaskel/scripts/skeleton/skel/scripts/ws.py
@@ -0,0 +1,45 @@
+try:
+    from gevent import monkey
+
+    monkey.patch_all()
+except (ImportError, ModuleNotFoundError):
+    try:
+        from eventlet import monkey_patch
+
+        monkey_patch()
+    except (ImportError, ModuleNotFoundError):
+        pass
+
+from typing import Dict
+
+import click
+
+from flaskel import AppBuilder, middlewares as middle
+from flaskel.ext.websocket import socketio
+
+EXTENSIONS: Dict[str, Dict] = {}
+
+APP_CONFIG = dict(
+    static_folder=None,
+    extensions=EXTENSIONS,
+    middlewares=(
+        middle.RequestID,
+        middle.ReverseProxied,
+    ),
+)
+
+
+def get_app():
+    return AppBuilder(**APP_CONFIG).get_or_create()
+
+
+@click.command()
+@click.option("-p", "--port", default=5000, type=int)
+@click.option("-H", "--bind", default="127.0.0.1")
+def cli(port, bind):
+    app = get_app()
+    socketio.run(app, host=bind, port=port, debug=app.debug, log_output=app.debug)
+
+
+if __name__ == "__main__":
+    cli()  # pylint: disable=no-value-for-parameter
