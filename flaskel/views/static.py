3a71b90ee07aeb9b51f895bd8136f21937d52943
cs91chris <cs91chris@voidbrain.me>
2021-09-10 21:34:53 +0200

    added static views

---
+++ flaskel/views/static.py
@@ -0,0 +1,31 @@
+from decouple import config
+from flask import send_from_directory
+
+from flaskel import cap, Response, httpcode
+from flaskel.views import BaseView
+
+
+class StaticFileView(BaseView):
+    default_static_path = "static"
+    default_view_name = "static"
+    default_urls = ("/static/<path:filename>",)
+
+    def dispatch_request(self, filename, *_, **__):  # pylint: disable=arguments-differ
+        return send_from_directory(self.default_static_path, filename)
+
+
+class SPAView(BaseView):
+    template = "index.html"
+    default_view_name = "spa"
+    default_static_folder = config("SPA_STATIC_FOLDER", default="webapp")
+    default_static_url_path = config("SPA_STATIC_URL_PATH", default="webapp")
+
+    default_urls = (
+        "/",
+        "/<path:path>",
+    )
+
+    def dispatch_request(self, *_, path=None, **__):  # pylint: disable=arguments-differ
+        if path is not None:
+            return Response.no_content(httpcode.NOT_MODIFIED)
+        return cap.send_static_file(self.template)
