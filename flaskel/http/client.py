0d491e914b3c6bc30b6ed74627dced019912a34b
cs91chris <cs91chris@voidbrain.me>
2022-02-17 00:15:52 +0100

    refactored http package

--- flaskel/http/client.py
+++ flaskel/http/client.py
@@ -1,14 +1,37 @@
 from requests import exceptions as http_exc
+from vbcore.datastruct import Dumper
 from vbcore.http.batch import HTTPBatch
 from vbcore.http.client import HTTPClient, JsonRPCClient
+from vbcore.http.httpdumper import BaseHTTPDumper
 from vbcore.uuid import get_uuid
 
 from flaskel import cap, request
-from .httpdumper import FlaskelHTTPDumper
 
 HTTPStatusError = (http_exc.HTTPError,)
 NetworkError = (http_exc.ConnectionError, http_exc.Timeout)
 all_errors = (*HTTPStatusError, *NetworkError)
+
+
+class LazyHTTPDumper(BaseHTTPDumper):
+    @classmethod
+    def dump_request(cls, req, *args, **kwargs):
+        return Dumper(req, *args, callback=super().dump_request, **kwargs)
+
+    @classmethod
+    def dump_response(cls, resp, *args, **kwargs):
+        return Dumper(resp, *args, callback=super().dump_response, **kwargs)
+
+
+class FlaskelHTTPDumper(LazyHTTPDumper):
+    @classmethod
+    def dump_request(cls, req, *_, dump_body=None, **kwargs):
+        h = cap.config.LOG_REQ_HEADERS
+        return super().dump_request(req, dump_body, only_hdr=h, **kwargs)
+
+    @classmethod
+    def dump_response(cls, resp, *_, dump_body=None, **kwargs):
+        h = cap.config.LOG_RESP_HEADERS
+        return super().dump_response(resp, dump_body, only_hdr=h, **kwargs)
 
 
 class FlaskelHTTPBatch(FlaskelHTTPDumper, HTTPBatch):
