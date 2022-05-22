0735951749763cec0f8936c9499e553c8397bc33
cs91chris <cs91chris@voidbrain.me>
2022-05-23 00:06:34 +0200

    removed legacy ipban

--- Makefile
+++ Makefile
@@ -26,6 +26,7 @@ all: clean run-tox
 lint: flake pylint mypy
 security: safety liccheck
 format: autoflake black isort
+dev: format lint test
 
 
 compile-deps:
--- tests/unit/ext/test_limit.py
+++ tests/unit/ext/test_limit.py
@@ -1,11 +1,9 @@
-from unittest.mock import patch
-
 import pytest
 from vbcore.http import httpcode
 from vbcore.tester.mixins import Asserter
 
 from flaskel import Response
-from flaskel.ext.limit import header_whitelist, ipban, response_ko, response_ok
+from flaskel.ext.limit import header_whitelist, response_ko, response_ok
 
 
 @pytest.mark.parametrize(
@@ -47,57 +45,3 @@ def test_bypass_limit(flaskel_app):
 
     with flaskel_app.test_request_context(headers={bypass_key: bypass_value}):
         Asserter.assert_true(header_whitelist())
-
-
-def test_ipban_init(flaskel_app):
-    obj = "flaskel.ext.limit.ipban"
-    with patch(f"{obj}.add_whitelist"), patch(f"{obj}.load_nuisances"):
-        ipban.init_app(flaskel_app)
-
-    Asserter.assert_true(flaskel_app.config.IPBAN_ENABLED)
-    Asserter.assert_equals(flaskel_app.config.IPBAN_STATUS_CODE, httpcode.FORBIDDEN)
-
-
-def test_ipban_is_whitelisted(flaskel_app):
-    ip = "127.0.0.1"
-    with flaskel_app.test_request_context():
-        ipban.add_whitelist(ip)
-        Asserter.assert_true(ipban.is_whitelisted(ip))
-        ipban.remove_whitelist(ip)
-        Asserter.assert_false(ipban.is_whitelisted(ip))
-        Asserter.assert_true(ipban.is_whitelisted(url="/robots.txt"))
-
-
-def test_load_nuisances():
-    loaded = ipban.load_nuisances(
-        {
-            "regex": [
-                r".*\.aspx$",
-                r".*\.php$",
-                r".*\.py$",
-                r".*\.sh$",
-                r".*\.sql$",
-                r"\/.*\/(calendar|date|fortune|redirect|passwd)$",
-                r".*?PHPSESSID=",
-                r"\/([sf]|web|ows|mp)cgi(-bin|)\/",
-                r"^/\.git",
-            ],
-            "string": [
-                "/index.php/admin/",
-                "/joomla/",
-                "/manager/html",
-                "/myadmin/",
-                "/mysqladmin/",
-                "/phpmyadmin/",
-                "/remote/login",
-                "/temp/wp-admin/",
-            ],
-            "ip": [
-                "8.8.8.8",
-                "1.1.1.1",
-                "2.2.2.2",
-            ],
-        }
-    )
-
-    Asserter.assert_equals(loaded, 20)
